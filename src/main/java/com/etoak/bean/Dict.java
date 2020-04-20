c90a10c020d1bcfe5061d9e67eeca0d63a85859e
xiaxinxu <780101313@qq.com>
2020-04-20 14:59:08 +0800

    div分支 Dict.java

---
+++ src/main/java/com/etoak/bean/Dict.java
@@ -0,0 +1,14 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class Dict {
+
+    private Integer id;
+    private String groupId;
+    private String name;
+    private String value;
+    private Integer sort;
+
+}

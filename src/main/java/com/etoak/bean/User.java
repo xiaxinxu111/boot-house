08ac06b80000c52655bc13c660173e1510eb7ae1
xiaxinxu <780101313@qq.com>
2020-04-20 14:54:16 +0800

    dev分支文件

---
+++ src/main/java/com/etoak/bean/User.java
@@ -0,0 +1,15 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private Integer id;
+    private String name;
+    private String password;
+    private String gender;
+    private Integer age;
+    private String birthday;
+
+}

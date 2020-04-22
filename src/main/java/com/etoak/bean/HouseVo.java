a4c984f3652bda2cf4d5364b7301ed0561011414
xiaxinxu <780101313@qq.com>
2020-04-22 17:18:31 +0800

    房源查询完成一部分

---
+++ src/main/java/com/etoak/bean/HouseVo.java
@@ -0,0 +1,12 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+@Data
+public class HouseVo extends  House{
+
+    private String rentModeName;
+    private String houseTypeName;
+    private String orientationName;
+
+}
